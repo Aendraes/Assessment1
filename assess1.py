3b62a5f5acd9e2a81911d08d58a6f2acb73dc244
Aendraes <aendraes931@gmail.com>
2022-12-06 10:17:24 +0100

    3.4

--- assess1.py
+++ assess1.py
@@ -1,8 +1,12 @@
 import psycopg2
 
+
+
+# 3.3 Add to the program a function that creates the connection to the database. 
+# Commit to repo. (This is part of Task 2.3) 
 def psconnect():
     params = {
-        'dbname': 'website_database',
+        'dbname': 'assessmentcontacts',
         'user': 'postgres',
         'password': '',
         'host': 'localhost',
@@ -11,3 +15,21 @@ def psconnect():
 
     connection = psycopg2.connect(**params)
     return connection
+
+# 3.4 Create a function that can fetch data from the database and print it to the 
+# console. Commit to remote repo. 
+def pslist(query):
+    connection = psconnect()
+    cursor = connection.cursor()
+    cursor.execute(query)
+    list_for_printing = cursor.fetchall()
+    for item in list_for_printing:
+        print(item)
+
+    return None
+
+if __name__ == '__main__':
+    query = """
+    SELECT * FROM CONTACTS;
+    """
+    pslist(query)
